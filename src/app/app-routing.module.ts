d8f385358cf2aaad19960e0900b452307b0c5793
CristinaJordi <cristina.jordi@bluewin.ch>
2023-07-31 22:50:42 +0200

    Addes user-detail component and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'users/:id', component: UserComponent },
+  { path: 'users/:id', component: UserDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'user', component: UserComponent }
-
 ];
 
 @NgModule({
